4df19c93795ab8e1c79cd7ca41c8dbf026ca2887
kendyng2405 <nguyenk3558@my.uwstout.edu>
2026-03-27 02:55:58 +0700

    Update ChatbotController.js

--- assets/js/controllers/ChatbotController.js
+++ assets/js/controllers/ChatbotController.js
@@ -100,7 +100,7 @@ export class ChatbotController {
       }
     };
 
-    const res = await fetch("http://localhost:3000/api/chat", {
+    const res = await fetch(GEMINI_API_URL, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(body),
