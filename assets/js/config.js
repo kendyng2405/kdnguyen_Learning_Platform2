6c0acb0a35bccc306d54f5378029ba3805cb70b1
kendyng2405 <nguyenk3558@my.uwstout.edu>
2026-03-27 11:38:35 +0700

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -12,7 +12,7 @@ export const firebaseConfig = {
 };
 
 export const GEMINI_API_KEY = "AIzaSyA8MdW9LtNOBqLTY0fofF6Tu7IvS0ppFFg"
-export const GEMINI_API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${GEMINI_API_KEY}`;
+export const GEMINI_API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${GEMINI_API_KEY}`;
 
 export const APP_CONFIG = {
   name: "KDLearnSpace",
